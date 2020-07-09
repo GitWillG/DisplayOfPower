046840cdda7a7c9ce11b671aa293700916effa42
JunoP04 <50187932+JunoP04@users.noreply.github.com>
2020-07-09 16:22:02 -0400

    june 9th 2020

---
+++ Display Of Power/Assets/BestManJuno/BestScripts/EnemySpawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GenerateGrid generateGrid;
+
+    [ContextMenu("Spawn Enemies")]
+    public void SpawnEnemies()
+    {
+        generateGrid.hexPrefab
+    }
+}
