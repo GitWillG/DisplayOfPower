06b95f386e5eb92a03936f4550911401e0b3f510
EfeKaracar <efekaracar@gmail.com>
2020-08-12 15:58:44 -0700

    Commented some of the code
    Commented some of the code, animation system redone,

--- Display Of Power/Assets/Efe/Scripts/immersionManager.cs
+++ Display Of Power/Assets/Efe/Scripts/immersionManager.cs
@@ -1,32 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using efe;
+using efe; // Access to namespace library
+
+
+// Immersion manager governs everything that doesn't affect the game design/gameplay directly, but used mostly for visualization.
+// Features such as overhead names are written here.
+// Efe Karacar
 
 public class immersionManager : MonoBehaviour
 {
 
+    // 3D text that spawns on top of actors' heads
     public GameObject NPCObject;
     actorData data;
     gameManager gm;
     GameObject curPlayer;
-
+    // Minimum distance between players and all legit actors required for labels to appear
     public float distance_NPC_labels;
-
+    // Static metric gathered from basemesh to spawn the 3D text on top of actor's head
     public float Y_base_label_pos;
-
+    // List to store all active labels in the scene
     public List<GameObject> labelsActive;
-
+    // Material for edge outline
     public Material selectedMaterialVFX;
-
+    // Before using edge outline, materials of hovered objects need to be stored
+    // TODO - might have to store it on instances instead if more than 2 objects can be hovered
     Material[] selected_object_materials;
+    // 3D exclamation model used to show if an actor has any quest to give to player
     public GameObject questAvailableMark;
 
     void Start()
     {
+        // Initialize declarations
         distance_NPC_labels = 5;
         Y_base_label_pos = 18;
 
+        // Reference to gamemanager
         gm = GetComponent<gameManager>();
 
         curPlayer = gm.curAvatar;
@@ -38,43 +48,59 @@ public class immersionManager : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.H))
         {
+            // Find all the actors in the scene
             GameObject[] NPCs = GameObject.FindGameObjectsWithTag("NPC");
+            // Access each actor
             foreach(GameObject NPC in NPCs)
             {
                 data = NPC.GetComponent<actorData>();
+                // If any actor in this scene is peaceful
                 if(data.peaceful == true)
                 {
+                    // Store its distance to player
                     float distance = Vector3.Distance(curPlayer.transform.position, NPC.transform.position);
+                    // If it is below the declared value
                     if(distance < distance_NPC_labels)
-                    {
+                    {   
+                        // If it already doesnt have a label
                         if(data.responsibleLabelText == null)
                         {
+                            // Determine where to spawn
                             Vector3 spawnPosition = new Vector3(NPC.transform.position.x, Y_base_label_pos, NPC.transform.position.z);
+                            // Spawn the exclamation
                             GameObject newLabel = Instantiate(NPCObject, spawnPosition, Quaternion.identity);
+                            // Save it on the declared list
                             labelsActive.Add(newLabel);
-                            
-                            Debug.Log("Labels open. " + newLabel.name);
-
+                            // Change the text of 3d mesh to actorName which is stored in actorData
                             newLabel.GetComponent<TextMesh>().text = data.actorName;
+                            // Store the 3d object in each instanced actor
                             data.responsibleLabelText = newLabel;
+                            // Make sure exclamation follows the actor
                             newLabel.transform.parent = NPC.transform;
                         }
                     }
                 }
             }
         }
+
+        // Remove all labels in the scene when you let the same key go
         if(Input.GetKeyUp(KeyCode.H))
         {
+            // Access to the list where all labels are stored once pressed H
             foreach(GameObject temp in labelsActive)
             {
+                // Destroy
                 Destroy(temp.gameObject);
+                // Refresh the list
                 labelsActive.Remove(temp);
-                Debug.Log("Labels closed. " + temp.name);
+                // Debug.Log("Labels closed. " + temp.name);
 
             }
         }
     }
 
+    // Script responsible switching between an object's original materials and an edge outline material once hovered
+    // Called from mouseControls
     public void highlighObject(GameObject target)
     {
         selected_object_materials = target.GetComponent<Renderer>().materials;
--- Display Of Power/Assets/Efe/Scripts/mouseControls.cs
+++ Display Of Power/Assets/Efe/Scripts/mouseControls.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using TMPro;
+using efe; // Access to library, namespaces need to be referenced like this
+
+// This script governs every mechanic related to mouse and its links to the game. 
+// all clicking behaviours are also governed here.
+// Efe Karacar
+
 // needs to be called "using efe;"
-namespace efe
+namespace efe // efe library
 {
     public class mouseControls : MonoBehaviour
     {
@@ -17,24 +23,19 @@ namespace efe
         locationData locData;
         immersionManager im;
         public questItem curQuest;
-
-        float curSpeed;
-        Vector3 targetMovePosition;
-
-        bool moving = false;
-        float initialRunSpeed = 1;
-        bool controllerSent = false;
-        public GameObject moveParticle;
+        public GameObject moveParticle; // Particle that will spawn on mouse click if player agent is suppposed to move somewhere
         // Start is called before the first frame update
         void Start()
         {
+            // Most of the managers are stored in same object as GM is stored in
             gm = GetComponent<gameManager>();
             guim = GetComponent<GUIManager>();
             im = GetComponent<immersionManager>();
             
-
+            // Avatar player uses in level or world?
             playerToControl = gm.curAvatar;
 
+            // Access to components to be used later
             curAgent = playerToControl.GetComponent<NavMeshAgent>();
             curAnimator = playerToControl.GetComponent<Animator>();
         }
@@ -42,9 +43,13 @@ namespace efe
         // Update is called once per frame
         void Update()
         {
+            // Stored ray
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // Result of the ray
             RaycastHit hit;
 
+            // Always make sure you are using the right avatar and restore if it is the wrong one
+            // Aka - referencing the avatar used in levels for world map behaviours
             if(playerToControl != gm.curAvatar)
             {
                 playerToControl = gm.curAvatar;
@@ -52,108 +57,118 @@ namespace efe
                 curAnimator = playerToControl.GetComponent<Animator>();
             }
 
+            // Cast
             if(Physics.Raycast(ray, out hit, 1000))
             {
+                // Instead of casting it twice, conditions can be put inside a single ray
                 if (Input.GetMouseButtonDown(0)) // left click
                 {
+                    // Script responsible of sending agent to vector3
                     movePlayer(curAgent, hit.point);
                 }
                 if(Input.GetMouseButtonDown(1)) // right click
                 {
+                    // Script responsible for interacting with any object in game - actors, items, props, locations...etc
                     interactObject(hit.transform.gameObject);
                 }
-            }
-
-            processPlayerMovement();
-            detectHover();
-        }
-
-        void detectHover()
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, 1000))
-            {
+                
+                // hover below
                 Debug.Log(hit.transform.gameObject.name);
+                // Ray is still casted, but no condition is needed as it is cast all the time
+                // TODO - Optimization, use coroutine or invoke to cast it less
+                // Now it is casted 24 times every second
+                // Hover over NPCs
                 if(hit.transform.gameObject.tag == "NPC")
                 {
+                    // Store
                     gm.curHoveredObject = hit.transform.gameObject;
+                    // Enable edge outline
                     im.highlighObject(gm.curHoveredObject);
                 }
             }
+
+            // Script responsible of syncing blend trees and navmesh agents
+            processPlayerMovement();
         }
 
 
-
+        // called from update above with raycast on mouse position
         public void movePlayer(NavMeshAgent objectSource, Vector3 targetPoint)
         {
+            // target vector3, move player agent
             objectSource.SetDestination(targetPoint);
-            targetMovePosition = targetPoint;
-
-            float speed = Vector3.Distance(playerToControl.transform.position, targetMovePosition);
-            initialRunSpeed = speed;
-            controllerSent = false;
-
-            GameObject vfx = Instantiate(moveParticle, targetMovePosition, Quaternion.identity);
+            // instantiate selection particle on agent's current destination he is travelling to
+            GameObject vfx = Instantiate(moveParticle, targetPoint, Quaternion.identity);
+            // destroy it after a second
             Destroy(vfx, 1);
-            // Debug.Log("Ordered to move.");
+            
         }
 
         void processPlayerMovement()
-        {
-            float distance = Vector3.Distance(playerToControl.transform.position, targetMovePosition);
-            // Debug.Log(distance);
-            if(distance > 2)
-            {   
-                if(!controllerSent)
-                {
-                    curAnimator.SetFloat("Speed", initialRunSpeed);
-                    controllerSent = true;
-                }
-                else if(controllerSent == true)
-                {
-                    initialRunSpeed--;
-                    curAnimator.SetFloat("Speed", initialRunSpeed);
-                }
+        {       
 
-                // Debug.Log("Moving");
-                    if(distance < 2)
-                    {   
-                        curAnimator.ResetTrigger("Speed");
-                        // Debug.Log("Reached");
-                    }
+            // uses the navmesh system to grab the velocity of the player - aka his current speed
+            // agent.speed returns maximum possible agent can reach
+            // compare it with stoppind distance
+            if(curAgent.remainingDistance > curAgent.stoppingDistance)
+            {
+                // blend trees sync with navmesh
+                // Animation controller has a float called "Speed", which blend tree inside also uses
+                // Blend tree uses it to determine thresholds to determine which animation should play
+                // Aka - 0.5 speed = walk, 1 = run, 0 = idle
+                curAnimator.SetFloat("Speed", curAgent.velocity.magnitude);
+            }     
+            else
+            {
+                // still blend as it will send 0 for magnitude anyhow
+                // else represents that agent reached its target vector
+                curAnimator.SetFloat("Speed", curAgent.velocity.magnitude);
             }
         }
 
+        // Script to govern mouse clicks (right or wrong) on gameobjects
+        // It is called from update above
         public void interactObject(GameObject targetObject)
         {
+            // Save the last target always
             gm.lastSelectedTarget = targetObject;
+            // Actors of all kind
             if(targetObject.tag == "NPC")
             {
+                // Reference to actordata
                 actorData = targetObject.GetComponent<actorData>();
+                // Actors that wont fight aka random villagers walking around, quest givers, important figures to talk to
                 if(actorData.peaceful == true)
                 {
                     Debug.Log("Clicked on a peaceful NPC.");
+                    // Open the dialog popup
                     GameObject newScreen = Instantiate(guim.dialogScreen, new Vector2(Screen.width / 2, Screen.height / 2), Quaternion.identity);
+                    // Get the actor's quest
                     curQuest = actorData.actorQuests[0];
+                    // Transfer actor's quest to the popup
                     newScreen.GetComponent<menuScripts>().questName_gui.GetComponent<TextMeshProUGUI>().text = curQuest.questName;
                     newScreen.GetComponent<menuScripts>().questDesc_gui.GetComponent<TextMeshProUGUI>().text = curQuest.questDesc;
                     newScreen.GetComponent<menuScripts>().questGold_gui.GetComponent<TextMeshProUGUI>().text = curQuest.questGold.ToString();
                     newScreen.GetComponent<menuScripts>().questXP_gui.GetComponent<TextMeshProUGUI>().text = curQuest.questXp.ToString();
                 }
             }
+            // World map locations
             else if(targetObject.tag == "Location")
             {
+                // The script reference in which all data regarding locations are stored
                 locData = targetObject.GetComponent<locationData>();
+                // If location is a city, village - some place where there wont be encounters
                 if(locData.peaceful == true)
                 {
+                    // Store player's current location in world
                     gm.curLocation = targetObject;
+                    // Open interactive page with options like siege, enter the location...etc
                     GameObject newScreen = Instantiate(guim.partyEntrance_GUI, new Vector2(Screen.width / 2, Screen.height / 2), Quaternion.identity);
-                    Debug.Log("Clicked on a peaceful location.");
+                    // Debug.Log("Clicked on a peaceful location.");
                 }
             }
             
-            Debug.Log("Interacted with " + targetObject.name);
+            // Debug.Log("Interacted with " + targetObject.name);
         }
     }
 
