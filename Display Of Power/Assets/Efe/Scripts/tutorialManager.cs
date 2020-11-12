61c4ad8151c02a45e9ce1a11fccb82a1521d7b7b
EfeKaracar <efekaracar@gmail.com>
2020-11-12 14:23:32 -0500

    tutorialManager, onboarding part 1
    tutorialManager, onboarding part 1

---
+++ Display Of Power/Assets/Efe/Scripts/tutorialManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialManager : MonoBehaviour
+{
+    public bool isTutorialOn;
+    public GameObject alignerChecklist;
+    public GameObject questTemplate_GUI;
+
+    public void startTutorial()
+    {
+        isTutorialOn = true;
+    }
+
+    public void endTutorial()
+    {
+        isTutorialOn = false;
+    }
+}
